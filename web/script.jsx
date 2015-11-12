1541ab7f944261ea9fac6d6e11902ebc81e99489
techbot <djtechbot@gmail.com>
2015-11-12 16:07:38 +0100

    adds increment

--- web/script.jsx
+++ web/script.jsx
@@ -1,8 +1,13 @@
 var Button = React.createClass({
-
-    render:function() {
+    getInitialState:  function() {
+        return {counter: 0}
+    },
+    handleClick: function(){
+        this.setState({ counter: this.state.counter+1 })
+    },
+    render: function() {
         return  (
-         <button>Go</button>
+         <button onClick={this.handleClick}>{this.state.counter}</button>
         )
     }
 });
