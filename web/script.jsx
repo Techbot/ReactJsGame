9d021aeda37d4e736a092fc250fa39b5ccb1bc62
techbot <djtechbot@gmail.com>
2015-11-12 17:24:30 +0100

    adds more buttons. (I had to access prop via target.innerHTML

--- web/script.jsx
+++ web/script.jsx
@@ -1,15 +1,44 @@
 var Button = React.createClass({
-    getInitialState:  function() {
-        return {counter: 0}
-    },
-    handleClick: function(){
-        this.setState({ counter: this.state.counter+1 })
+
+    localHandleClick:function(){
+        this.props.localHandleClick(this.props.increment);
     },
     render: function() {
         return  (
-         <button onClick={this.handleClick}>{this.state.counter}</button>
+         <button onClick={this.props.localHandleClick}>{this.props.increment}</button>
+        )
+    }
+
+});
+
+var Result = React.createClass({
+    render: function() {
+        return  (
+            <div>{this.props.localCounter}</div>
         )
     }
 });
-React.render(<Button />,document.getElementById("root"));
 
+var Main = React.createClass({
+    getInitialState: function () {
+        return {counter: 0}
+    },
+    handleClick: function (increment) {
+        console.log(increment.target.innerHTML);
+        this.setState({counter: this.state.counter + parseInt(increment.target.innerHTML) });
+    },
+    render: function () {
+        return (
+            <div>
+                <Button localHandleClick={this.handleClick} increment='1'/>
+                <Button localHandleClick={this.handleClick} increment='5'/>
+                <Button localHandleClick={this.handleClick} increment='10'/>
+                <Button localHandleClick={this.handleClick} increment='100'/>
+
+                <Result localCounter={this.state.counter}/>
+            </div>
+        )
+    }
+});
+
+ReactDOM.render(<Main />,document.getElementById("root"));
