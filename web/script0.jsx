b80f1339ca84dd408d62f06477f45f851964548c
techbot <djtechbot@gmail.com>
2015-11-12 18:23:04 +0100

    adds template and original script

---
+++ web/script0.jsx
@@ -0,0 +1,12 @@
+var Main = React.createClass({
+
+    render: function () {
+        return (
+            <div>
+              ...
+            </div>
+        )
+    }
+});
+
+ReactDOM.render(<Main />,document.getElementById("root"));
