5932d5cb17892e863176abf91e5ddba95928f74a
techbot <djtechbot@gmail.com>
2015-11-12 15:49:34 +0100

    adds babel

---
+++ web/script.jsx
@@ -0,0 +1,10 @@
+var Button = React.createClass({
+
+    render:function() {
+        return  (
+            <h1>Hello!</h1>
+        )
+    }
+});
+React.render(<Button />,document.getElementById("root"));
+
